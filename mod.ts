d6a393f30a7fe70fd3fe81bb9891f9a3406e49f6
matt flamm <matt@flamm.dev>
2022-03-12 16:31:13 -0700

    add frontend code; begin moving dependancies; change what is served to frontend; add basic error handling;

--- mod.ts
+++ mod.ts
@@ -1,18 +1,54 @@
-import {Application} from "https://deno.land/x/oak@v10.4.0/mod.ts"
-import {getArt} from "./utilities.ts"
+import {Application, send} from "https://deno.land/x/oak@v10.4.0/mod.ts";
+import * as log from "https://deno.land/std@0.129.0/log/mod.ts";
+import { join } from "https://deno.land/std@0.128.0/path/mod.ts";
+import {getArt} from "./utilities.ts";
 
 
 const app = new Application();
 const PORT = 8000; //shouldn't this be an env variable somewhere?
 
-let art: string;
+//error handling and metrics
+app.use(async (context, next) => {
+  const start = Date.now();
+  log.info(`request received ${context.request.method}  ${context.request.url}`);
+  try {  //wrapping everyting in a try catch means errors will bubble up to here
+    await next();
+    const delta = Date.now() - start;
+    log.info(`request completed in ${delta}ms`);
+    context.response.headers.set("X-Response-Time", `${delta}ms`);
+  } catch (e) {
+    log.error(e);
+    context.response.body ="An internal error occured";
+    throw e;
+  }
 
-app.use((context, next) => {
+});
+
+app.use(async (context) => {
+  const filePath = context.request.url.pathname ? context.request.url.pathname : "/index.html";
+  const fileWhiteList = [
+    "/index.html",
+    "/javascripts/script.js",
+    "/stylesheets/style.css",
+    "/images/favicon.png"
+  ];
+  if (fileWhiteList.includes(filePath)) {
+    await send(context, filePath, {
+      root: join(Deno.cwd(), "public")
+    });
+  } else {
+    context.throw(404, "resource not found");
+  }
+  
+});
+
+app.use((context) => {
   context.response.body = getArt;
 });
 
 
 if (import.meta.main) { //check if running directly or as import
+  log.info("starting the app");
   await app.listen({
     port: PORT
   });
